73140ddd0b7db3fb79ad4ea91f668f6260155d42
nafiulaziz <nafiulaziz.na@gmail.comm>
2025-08-04 21:10:43 +0600

    Upload the Providers file

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -15,6 +15,13 @@ class AuthServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        $this->registerPolicies();
+        $this->routes(function () {
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/api.php'));
+    
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+        });
     }
 }
