65551f112bdd7ce8f2c0beadd9c881733ec888df
nafiulaziz <nafiulaziz.na@gmail.comm>
2025-08-04 16:08:39 +0600

    Add BookingPolicy

---
+++ app/Policies/BookingPolicy.php
@@ -0,0 +1,25 @@
+<?php
+// app/Policies/BookingPolicy.php
+
+namespace App\Policies;
+
+use App\Models\Booking;
+use App\Models\User;
+
+class BookingPolicy
+{
+    public function view(User $user, Booking $booking)
+    {
+        return $user->isAdmin() || $user->id === $booking->user_id;
+    }
+
+    public function update(User $user, Booking $booking)
+    {
+        return $user->isAdmin() || $user->id === $booking->user_id;
+    }
+
+    public function delete(User $user, Booking $booking)
+    {
+        return $user->isAdmin() || $user->id === $booking->user_id;
+    }
+}
