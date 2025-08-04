0d74b724d4ea83cdd1a7718283bfc796dc200cea
nafiulaziz <nafiulaziz.na@gmail.comm>
2025-08-04 20:26:47 +0600

    Update the Service Factory.php file

---
+++ resources/views/services.blade.php
@@ -0,0 +1,8 @@
+<!-- resources/views/services.blade.php -->
+@extends('layouts.app')
+
+@section('content')
+<div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <services-page></services-page>
+</div>
+@endsection
